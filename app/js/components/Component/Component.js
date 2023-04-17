d22bda2b4cc4bd357d774a37f2fcdd403470e317
Nary Lozano <narylozano@gmail.com>
2023-04-17 22:21:57 +0200

    Add component class to create html elements

---
+++ app/js/components/Component/Component.js
@@ -0,0 +1,9 @@
+class Component {
+  element;
+  constructor(parentElement, tagName) {
+    this.element = document.createElement(tagName);
+    parentElement.appendChild(this.element);
+  }
+}
+
+export default Component;
